2c0c4aebd437a4f3cb7039aa52101f841779a1c0
Z22CLABO <constance.laborie.partner@decathlon.com>
2019-05-22 10:59:03 +0200

    test cors

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -78,12 +78,8 @@ import moment from 'moment'
 
 const config = {
     headers: { 
-      'Access-Control-Allow-Origin': '*',
-	  'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-	  'Content-Type': 'application/json',
-	  'Access-Control-Allow-Methods': '*',
-	  'Access-Control-Max-Age': '2592000',
-	  'Access-Control-Allow-Credentials': 'true',
+	  'Access-Control-Request-Headers': 'Content-Type, Authorization',
+	  'Access-Control-Request-Method': 'GET',
     }
 };
 
